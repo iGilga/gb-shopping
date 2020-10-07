251d3087c73bc025fd2a164d84a478bbeb8f1142
Gilga <pyua87@gmail.com>
2020-10-08 00:14:52 +0300

    Getting list good with api

--- tests/unit/goodService.spec.js
+++ tests/unit/goodService.spec.js
@@ -2,19 +2,21 @@ import GoodService from '@/services/goodService';
 
 describe('goodService', () => {
   const expectData = [
-    { id: 1, title: 'Shirt', price: 150 },
-    { id: 2, title: 'Socks', price: 50 },
-    { id: 3, title: 'Jacket', price: 350 },
-    { id: 4, title: 'Shoes', price: 250 },
-    { id: 5, title: 'Shoes', price: 300 },
-    { id: 6, title: 'Shoes', price: 75 },
-    { id: 7, title: 'Shoes', price: 275 },
-    { id: 8, title: 'Shoes', price: 400 },
+    {
+      id_product: 123,
+      product_name: 'Ноутбук',
+      price: 45600,
+    },
+    {
+      id_product: 456,
+      product_name: 'Мышка',
+      price: 1000,
+    },
   ];
-  it('fetching goods', () => {
+  it('fetching goods', async () => {
     const goods = GoodService.fetchGoods();
-    goods.then((data) => {
-      expect(data).toStrictEqual(expectData);
+    await goods.then((response) => {
+      expect(response.data).toStrictEqual(expectData);
     });
   });
 });
