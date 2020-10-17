4247c0fd000a4ae661ba64b0e8377698c02d1792
Gilga <pyua87@gmail.com>
2020-10-18 00:49:40 +0300

    Add search in menu

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -2,14 +2,24 @@
   <b-navbar oggleable="lg" type="dark" variant="info">
     <b-navbar-brand href="#">Shopping</b-navbar-brand>
     <b-navbar-nav class="ml-auto">
-      <b-button>Cart</b-button>
+      <div class="mr-sm-2">
+        <Search/>
+      </div>
+      <div>
+        <b-button>Cart</b-button>
+      </div>
     </b-navbar-nav>
   </b-navbar>
 </template>
 
 <script>
+import Search from './Search.vue';
+
 export default {
   name: 'Menu',
+  components: {
+    Search,
+  },
 };
 </script>
 
